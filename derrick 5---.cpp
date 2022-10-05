482c04b228c95f9ac9474ea484794199357f5ef7
derreckmasibo <114052254+derreckmasibo@users.noreply.github.com>
2022-10-05 02:07:38 -0700

    Add files via upload

---
+++ derrick 5---.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=30;i>=5;i--){
+        cout<<i<<" ";
+    }
+    return 0;
+}
